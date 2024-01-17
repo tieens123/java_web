2bbc345de3cef76dba8f95b5af2fb2270c31d0a3
tieens123 <tienhss2k3@gmail.com>
2024-01-17 21:49:17 +0700

    lab02

---
+++ src/main/java/test/servlet/HelloServlet.java
@@ -0,0 +1,57 @@
+package test.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletOutputStream;
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletOutputStream out = response.getOutputStream();
+		out.println("<html>");
+		out.println("<head><title>HelloServlet</title></head>");
+		out.println("<body>");
+		out.println("<p>This is my first Servlet</p>");
+		out.println("<p><a href= 'https://devmaster.edu.vn'>");
+		out.println("Devmaster Academy</a> </p>");
+		out.println("</body>");
+		out.println("<html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
